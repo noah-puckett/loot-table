16e8f18958065d18e9d633413f292b38989afffe
Noah Puckett <noah.ingram.puckett@gmail.com>
2020-04-29 13:34:25 -0700

    rendering test data

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-function randomItem(table){
-    return table.items[Math.random() * table.size];
-}
+import { dummyData } from './data/dummyData.js';
+import { getRandomItem } from './utils/helperFunctions.js';
 
+const root = document.getElementById('root');
+const ul = document.createElement('ul');
+
+dummyData.forEach(monster => {
+
+    const li = document.createElement('li');
+    const randomItem = getRandomItem(monster.lootTable).treasureName;
+
+    li.textContent = `${monster.monsterRace} has dropped 1 ${randomItem}`;
+    ul.appendChild(li);
+})
+
+root.appendChild(ul);
