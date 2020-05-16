507bc3b2f0766d04828d0c57af301d935d6eadf1
Noah Puckett <noah.ingram.puckett@gmail.com>
2020-05-15 18:22:06 -0700

    making some nice css to test with

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import { dummyData } from './data/dummyData.js';
-import { getRandomItem } from './utils/helperFunctions.js';
+import { getRandomItem, displayLootItem } from './utils/helperFunctions.js';
 
 const root = document.getElementById('root');
 const ul = document.createElement('ul');
+const refreshButton = document.createElement('button');
 
 dummyData.forEach(monster => {
-
-    const li = document.createElement('li');
-    const randomItem = getRandomItem(monster.lootTable).treasureName;
-
-    li.textContent = `${monster.monsterRace} has dropped 1 ${randomItem}`;
+    const randomItem = getRandomItem(monster.lootTable);
+    const li = displayLootItem(monster.monsterRace, randomItem);
     ul.appendChild(li);
 })
 
+
+refreshButton.textContent = 'Re-Randomize!';
+refreshButton.onclick = () => { location.reload() }
+
 root.appendChild(ul);
+root.appendChild(refreshButton);
--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -1,3 +1,26 @@
 export const getRandomItem = (lootTable) => {
     return lootTable[Math.floor(Math.random() * Math.floor(lootTable.length))];
 }
+
+export const displayLootItem = (monsterType, lootItem) => {
+    const parentEl = document.createElement('li');
+    parentEl.className = 'monster-card';
+
+    const monsterTitle = document.createElement('h1');
+    monsterTitle.textContent = monsterType;
+    parentEl.appendChild(monsterTitle);
+
+    const itemTitle = document.createElement('h3');
+    itemTitle.textContent = `Item Name: ${lootItem.treasureName}`;
+    parentEl.appendChild(itemTitle);
+
+    const itemDesc = document.createElement('p');
+    itemDesc.textContent = `Description: ${lootItem.treasureDesc}`;
+    parentEl.appendChild(itemDesc);
+
+    const itemVal = document.createElement('p');
+    itemVal.textContent = `Value: ${lootItem.treasureVal}`;
+    parentEl.appendChild(itemVal);
+
+    return parentEl;
+}
