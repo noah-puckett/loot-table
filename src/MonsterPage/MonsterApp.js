b22f922c32fc85c10f8d5b2741465f81f76d6c90
Noah Puckett <noah.ingram.puckett@gmail.com>
2020-05-25 22:38:36 -0700

    added app state for re-rendering of dom on monster submission

--- src/MonsterPage/MonsterApp.js
+++ src/MonsterPage/MonsterApp.js
@@ -1,20 +1,40 @@
 import Component from '../Component.js';
 import List from '../List.js';
-import { fetchFromApi, displayMonsterItem } from '../../utils/helperFunctions.js'; 
+import MonsterForm from './MonsterForm.js';
+import { fetchFromApi, sendToApi } from '../../utils/apiHelpers.js'; 
+import { displayMonsterItem } from '../../utils/helperFunctions.js';
 
 
 class MonsterApp extends Component {
-    //App should eventually just render the pages themselves
     
     render() {
-        const dom = this.renderDOM();
 
-        const monsterList = new List({ listItems: [], displayItem: displayMonsterItem });
-        dom.appendChild(monsterList.render());
+        const state = {
+            listItems: []
+        };
+
+        const dom = this.renderDOM();
+        
+        const addNewMonsterAndUpdate = (formData) => {
+            sendToApi('http://localhost:8000/monsters', 'POST', formData)
+                .then(monster => { 
+                    state.listItems.push(monster);
+                    monsterList.update({ listItems: state.listItems });
+                });
+        };
 
         fetchFromApi('http://localhost:8000/monsters')
-            .then(monsters => monsterList.update({ listItems: monsters }));
-
+            .then(monsters => {
+                state.listItems = monsters; 
+                monsterList.update({ listItems: monsters });
+            });
+        
+        const monsterForm = new MonsterForm({ handleSubmit: addNewMonsterAndUpdate });
+        dom.appendChild(monsterForm.render());
+        
+        const monsterList = new List({ listItems: [], displayItem: displayMonsterItem });
+        dom.appendChild(monsterList.render());
+        
         return dom;
     }
 
